e9b3b262facf009b7f9b18d8e72638f274b51265
alexey-mr <alexey.morlang@gmail.com>
2019-04-24 16:20:35 +0300

    Fixed pypi proxy creation

--- tfCIRepositories.groovy
+++ tfCIRepositories.groovy
@@ -74,26 +74,26 @@ create_docker_proxy('access', 5010, 'https://registry-1.docker.io')
 
 // PyPI
 // Proxy
-create_pypi_proxy(pypi, 'https://pypi.org/')
+create_pypi_proxy('pypi', 'https://pypi.org')
 
 // Raw
 // Proxy
-create_raw_proxy('ubuntu', 'http://mirror.yandex.ru/ubuntu/')
+create_raw_proxy('ubuntu', 'http://mirror.yandex.ru/ubuntu')
 
 // Yum
 // Proxy
-create_yum_proxy('docker-ce-stable',    'https://download.docker.com/linux/centos/7/x86_64/stable/')
+create_yum_proxy('docker-ce-stable',    'https://download.docker.com/linux/centos/7/x86_64/stable')
 create_yum_proxy('openstack-newton',    'http://mirror.neu.edu.cn/centos/7/cloud/x86_64/openstack-newton')
-create_yum_proxy('openstack-ocata',     'http://mirror.centos.org/centos/7/cloud/x86_64/openstack-ocata/')
+create_yum_proxy('openstack-ocata',     'http://mirror.centos.org/centos/7/cloud/x86_64/openstack-ocata')
 create_yum_proxy('openstack-queens',    'http://mirror.centos.org/centos/7/cloud/x86_64/openstack-queens')
 create_yum_proxy('openstack-rocky',     'http://mirror.centos.org/centos/7/cloud/x86_64/openstack-rocky')
-create_yum_proxy('centos74',            'http://mirror.yandex.ru/centos/7/os/x86_64/')
-create_yum_proxy('centos74-updates',    'https://mirror.yandex.ru/centos/7/updates/x86_64/')
-create_yum_proxy('centos74-extras',     'https://mirror.yandex.ru/centos/7/extras/x86_64/')
-create_yum_proxy('centos',              'https://mirror.yandex.ru/')
-create_yum_proxy('epel',                'http://mirror.yandex.ru/epel/7/x86_64/')
+create_yum_proxy('centos74',            'http://mirror.yandex.ru/centos/7/os/x86_64')
+create_yum_proxy('centos74-updates',    'https://mirror.yandex.ru/centos/7/updates/x86_64')
+create_yum_proxy('centos74-extras',     'https://mirror.yandex.ru/centos/7/extras/x86_64')
+create_yum_proxy('centos',              'https://mirror.yandex.ru')
+create_yum_proxy('epel',                'https://mirror.yandex.ru/epel/7/x86_64')
 // TODO: to be made hosted
-create_yum_proxy('yum-tungsten-tpc', 'http://148.251.5.90/tpc')
+create_yum_proxy('yum-tungsten-tpc',    'http://148.251.5.90/tpc')
 
 // TODO: Hosted are not needed
 //yum-tpc-test
